afad4a0df6185f836a25cd17922524fff71e38ab
Camila <125229387+camilarib@users.noreply.github.com>
2023-06-23 19:03:23 -0300

    Add files via upload

---
+++ Atv09/notebook/Atv09.java
@@ -0,0 +1,38 @@
+/*
+IFPR - Campus Cascavel
+Curso: Informatica
+Diciplina: Programação Orientada à Objetos
+Professor: Nelson Bellincanta
+Aluna: Camila Ribeiro Gonçalves
+Data: 11/05/2023
+*/
+
+//import java.util.ArrayList;
+
+public class Atv09 { // inicio da classe atv09
+    public static void imprimirNumeros(int[] numeros) { // recebe um array de inteiros como parâmetro e imprime cada número na tela
+        for (int numero : numeros) { // inicio do for que irá percorrer o array numeros
+            System.out.print(numero + " "); //adiciona o numero + um espaço
+        }
+        System.out.println(); // print dos elementos 
+    }
+
+public static void somaNumeros(int[] numeros){ // inicio do método somaNumeros recebe um array de inteiros como parâmetro, e mostra o resultado na tela
+    int soma = 0; //recebe a soma do array
+        for (int numero : numeros) { // calcula a soma dos numeros
+            soma += numero;
+        } //fim do for
+        System.out.println("A soma dos números é: " + soma); //print da soma de todos os numeros
+}
+public static void main(String[] args) { //inicio do método main
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // array de inteiros com valores de 1 a 10 
+
+        System.out.println("Array inicial:"); //
+        imprimirNumeros(numeros); // chama o método imprimirNumeros e exibe os numeros iniciais
+
+        somaNumeros(numeros); // chama o método imprimirNumeros e exibe a soma dos numeros
+
+        //System.out.println("Array atualizado após a soma:"); //
+       // imprimirNumeros(numeros); //
+    } //fim do método main
+} // fim da classe Atv09
